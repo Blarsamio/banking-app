efcbd82caf58c4013fe6df79ded8ab9cb5453222
Patricio <patriciopherrero@gmail.com>
2024-08-21 11:52:51 +0200

    created card component, left and right navbars and minimal layout of homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,34 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName : 'Sam', lastName: 'Malone', email:'sammalone@gmail.com'}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            account={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        {/* recent transactions */}
+      </div>
+      <RightSideBar
+          user={loggedIn}
+          transactions={[]}
+          banks={[{currentBalance: 123.50}, {}]}
+        />
+    </section>
   )
 }
 
